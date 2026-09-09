2148c8303d9075c182dc73e8143667a81ce731ac
adzizee <adzizee@johorplantations.com>
2026-09-09 21:31:36 +0800

    update kelulusan1 and semakkelulusan1 pages

--- lesen/kelulusan1.aspx.vb
+++ lesen/kelulusan1.aspx.vb
@@ -1,13 +1,10 @@
 ﻿
 Imports System
-'Imports System.Collections.Generic
-'Imports System.Configuration
 Imports System.Data
 Imports System.Data.SqlClient
 Imports System.Drawing
 Imports System.Drawing.Imaging
 Imports System.IO
-'Imports System.Web.UI.WebControls
 Imports Microsoft.ReportingServices.Rendering.ExcelRenderer.ExcelGenerator.BIFF8
 
 Partial Class kelulusan1
